4a4634648db71ccf9ffa52fb823648d2f0b3a8b0
Kadir <116947551+serpikoz@users.noreply.github.com>
2023-12-11 21:51:52 +0300

    start

---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import Hero from "./Hero";
+import CustomerReviews from "./CustomerReviews";
+import Footer from "./Footer";
+import PopularProducts from "./PopularProducts";
+import Services from "./Services";
+import SpecialOffer from "./SpecialOffer";
+import SuperQuality from "./SuperQuality";
+import Subscribe from "./Subscribe";
+
+export {
+  Hero,
+  CustomerReviews,
+  Footer,
+  PopularProducts,
+  Services,
+  SpecialOffer,
+  SuperQuality,
+  Subscribe,
+};
